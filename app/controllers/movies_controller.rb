9e63552b51d03a490f2de377ffd58cb69606114b
EC2 Default User <ec2-user@ip-172-31-1-216.us-east-2.compute.internal>
2020-09-13 06:10:03 +0000

    question 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -40,8 +40,8 @@ class MoviesController < ApplicationController
     end
     
     # check if current values is not match with the session values and redirect them to correct value
-    if session[:ratings] != params[:ratings] || session[:sort] != params[:sort]
-      redirect_to movies_path(ratings: session[:ratings], sort: session[:sort])
+    if session[:ratings] != params[:ratings] || session[:sort_by] != params[:sort_by]
+      redirect_to movies_path(ratings: session[:ratings], sort: session[:sort_by])
     end
   end
 
